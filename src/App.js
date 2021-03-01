fd2dee601a2ead9ad664e9f01bd06d4dde9da0d2
wayne <juchuang123@gmail.com>
2021-03-01 21:06:17 +0900

    favouriteの削除機能を追加

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
 import AddFavourites from './components/AddFavourites';
+import RemoveFavourites from './components/RemoveFavourites';
 
 const App = () => {
 	const [movies, setMovies] = useState([]);
@@ -27,6 +28,14 @@ const App = () => {
 		setFavourites(newFavouriteList);
 	};
 
+  const removeFavouritesMovie = (movie) => {
+    const newFavouriteList = favourites.filter(
+      (favourite) => favourite.imdbID !== movie.imdbID
+    );
+
+    setFavourites(newFavouriteList);
+  };
+
 	useEffect(() => {
 		getMovieRequest(searchValue);
 	}, [searchValue]);
@@ -48,7 +57,11 @@ const App = () => {
 				<MovieListHeading heading='Favourites' />
 			</div>
 			<div className='row'>
-				<MovieList movies={favourites} favouriteComponent={AddFavourites} />
+				<MovieList
+          movies={favourites}
+          handleFavouritesClick={removeFavouritesMovie}
+          favouriteComponent={RemoveFavourites}
+        />
 			</div>
 		</div>
 	);
